bf050debe6ada6de705d35786570daaab9e19eab
escm1990 <escm.1990@gmail.com>
2020-12-14 22:22:18 -0600

    2020-12-14 commit de cambios a la app de las buenas nuevas

---
+++ LasBuenasNuevas2/app/src/main/java/com/escm/lbn/activities/NoRedActivity.kt
@@ -0,0 +1,11 @@
+package com.escm.lbn.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NoRedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_no_red)
+    }
+}
