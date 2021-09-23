f935c8ab86b339c17899a50b955f99d23e71f61b
escm1990 <escm.1990@gmail.com>
2021-09-23 16:32:09 -0600

    2021-09-23 Se creó un nuevo repositorio para el proyecto de las buenas nuevas

--- LasBuenasNuevas2/app/src/main/java/com/escm/lbn/mailer/Correo.kt
+++
@@ -1,5 +0,0 @@
-package com.escm.lbn.mailer
-
-data class Correo(val email:String = "DEFAULT",
-                  val nombre:String = "DEFAULT_NAME",
-                  val comentarios:String = "DEFAULT_COMMENT")
--- LasBuenasNuevas2/app/src/main/java/com/escm/lbn/mailer/MainViewInterface.kt
+++
@@ -1,6 +0,0 @@
-package com.escm.lbn.mailer
-
-interface MainViewInterface {
-    fun showProgress()
-    fun hideProgress()
-}
